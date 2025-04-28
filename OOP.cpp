de2d1acdba9fb6f268f8bea270e168553c63cf5c
Reyanii <indriyaniv9@gmail.com>
2025-04-28 15:56:52 +0800

    menambahkan library dan OOP

---
+++ OOP.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Hewan{
+    private:
+    string jenisKelamin;
+    public:
+    string tipe;
+    string umur;
+
+    void inputData() { 
+        cout << "masukan jenis kelamin = " << endl;
+        cin >> jenisKelamin;
+        cout << "masukan tipe = " << endl;
+        cin >> tipe;
+        cout << "masukan umur = " << endl;
+        cin >> umur;
+    }
+};
+int main() {
+    Hewan hwn;
+    hwn.inputData();
+    cout << "umurnya adalah " << hwn.umur << endl;
+    cout << "tipenya adalah " << hwn.tipe << endl;
+}
